f12b52da096231c78aced341e23fcb00cb8c7ea7
Kevin Botero <kbotero201@gmail.com>
2020-06-26 16:25:54 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,7 +6,3 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
 
-person = kevin
-lang = ruby
-
-introduction_with_language(person, lang)
